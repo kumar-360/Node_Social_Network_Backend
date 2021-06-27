0e0db31d77e7150c1ba29f681f3daf318196b86b
Kumar Shanu <kumarshanu@Kumars-MacBook-Pro.local>
2021-06-28 00:36:04 +0530

    Setup done

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const feedRoutes = require("./routes/feed");
+
+const app = express();
+
+app.use(express.json());
+
+// For tackling CORS error
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+});
+
+app.use("/feed", feedRoutes);
+
+app.listen(8080, () => console.log("Started listening on port 8080"));
